cc12a05b45c48a8df5992a0c95df75aad8996cd2
heliogabal <rhalbmann@gmail.com>
2017-06-23 14:28:34 +0200

    responsive tweaks

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -5,7 +5,7 @@
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<meta name=viewport content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
