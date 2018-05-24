2175d60c5e519f0a816e1ede960de4f982bd1cc1
heliogabal <rhalbmann@gmail.com>
2018-05-24 20:46:59 +0200

    list-style disc and remove Piwik

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -11,20 +11,5 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  <!-- Piwik -->
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//inglist.de/piwik/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 41]);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
-    <noscript><p><img src="//inglist.de/piwik/piwik.php?idsite=41" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
 </body>
 </html>
